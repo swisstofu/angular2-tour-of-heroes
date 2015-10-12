b46807e06e3565b83f0ef90e5abccb060fd6dcf4
Rado Kirov <radokirov@google.com>
2015-10-12 14:42:00 -0700

    move to viewBindings out of bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import {View, Component, CORE_DIRECTIVES} from 'angular2/angular2';
+import {View, Component, CORE_DIRECTIVES, bind} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Routes, APP_ROUTES} from './route.config';
+import {HeroService} from "./hero.service";
+import {
+    routerBindings,
+    LocationStrategy,
+    HashLocationStrategy
+} from 'angular2/router';
 
 @Component({
   selector : 'my-app',
   directives : [ CORE_DIRECTIVES, ROUTER_DIRECTIVES ],
   templateUrl : 'app/app.component.html',
-  styleUrls : ['app/app.component.css']
+  styleUrls : ['app/app.component.css'],
+  viewBindings: [
+    HeroService,
+    bind(LocationStrategy).toClass(HashLocationStrategy)
+  ]
 })
 @RouteConfig(APP_ROUTES)
 export class AppComponent {
