2ed5387c28fde8d9fdfbe31a145cc021d4bfca54
Rado Kirov <radokirov@google.com>
2015-10-19 10:56:56 -0700

    shorten heroes list.

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,10 @@
 import {Hero} from './hero.model';
 
 export var HEROES: Hero[] = [
-  {"id": 11, "name": "Mr. Nice", "retired": false},
-  {"id": 12, "name": "Narco", "retired": false},
-  {"id": 13, "name": "Bombasto", "retired": false},
-  {"id": 14, "name": "Celeritas", "retired": false},
-  {"id": 15, "name": "Magneta", "retired": false},
-  {"id": 16, "name": "RubberMan", "retired": true},
-  {"id": 17, "name": "Dynama", "retired": false},
-  {"id": 18, "name": "Dr IQ", "retired": false},
-  {"id": 19, "name": "Magma", "retired": false},
-  {"id": 20, "name": "Tornado", "retired": false}
+  {"id": 1, "name": "Mr. Nice", "retired": false},
+  {"id": 2, "name": "Component Man", "retired": false},
+  {"id": 3, "name": "Bombasto", "retired": false},
+  {"id": 4, "name": "Celeritas", "retired": false},
+  {"id": 5, "name": "Magneta", "retired": false},
+  {"id": 6, "name": "Dr. Scope", "retired": true}
 ];
